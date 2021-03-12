050dccbbdb9fb2be3ab6c1af9d5bfb6a3499bc8e
dayknlang <dayaneknlang@gmail.com>
2021-03-11 21:11:55 -0300

    Finalização do exercício

--- semana5/labegram-estilizacao-listas/src/App.js
+++ semana5/labegram-estilizacao-listas/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-//import styled from "styled-components"
+
 
 import Post from './components/Post/Post';
 import FotoPerfil1 from './imagens/perfil1.jpg';
@@ -10,68 +10,104 @@ import Post1 from './imagens/post1.jpg';
 import Post2 from './imagens/post2.jpg';
 import Post3 from './imagens/post3.jpg';
 
+import styled from 'styled-components'
+
+const InputPost = styled.input`
+  margin: 5px;
+`
+
+const ButtonAdd = styled.button`
+  background-color: transparent;
+  margin: 10px;
+`
+
+
 class App extends React.Component {
   state = {
-    posts: [
+    post: [
       {
-        nomeUsuario: 'Paulinha',
-        fotoUsuario: 'FotoPerfil1',
-        fotoPost: 'Post1',
+        nomeUsuario :'Paulinha',
+        fotoUsuario : FotoPerfil1 ,
+        fotoPost : Post1
       },
       {
-        nomeUsuario: 'João Pedro',
-        fotoUsuario: 'FotoPerfil2',
-        fotoPost: 'Post2',
+        nomeUsuario :'João Pedro',
+        fotoUsuario: FotoPerfil2,
+        fotoPost: Post2
       },
       {
-        nomeUsuario: 'Daenerys',
-        fotoUsuario: 'FotoPerfil3',
-        fotoPost: 'Post3',
-      },
+        nomeUsuario:'Daenerys',
+        fotoUsuario: FotoPerfil3,
+        fotoPost: Post3 
+      }
     ],
-  };
+
+    valorInputNome: '',
+    valorInputUsuario: '',
+    valorInputPost: ''
+  }
+
+  adicionarPost = () => {
+    const novoPost = {
+      nomeUsuario: this.state.valorInputNome,
+      fotoUsuario: this.state.valorInputUsuario,
+      fotoPost: this.state.valorInputPost
+    }
+
+    const novosPosts = [...this.state.post, novoPost]
+
+    this.setState({ post: novosPosts })
+  }
+
+  onChangeInputNome = (event) => {
+    this.setState({ valorInputNome: event.target.value })
+  }
+
+  onChangeInputUsuario = (event) => {
+    this.setState({ valorInputUsuario: event.target.value })
+  }
+
+  onChangeInputPost = (event) => {
+    this.setState({ valorInputPost: event.target.value })
+  }
 
   render() {
+    const listaPost = this.state.post.map((post) => {
+      return (
+        <Post key={post.nomeUsuario}
+          nomeUsuario={ post.nomeUsuario }
+          fotoUsuario={ post.fotoUsuario }
+          fotoPost={ post.fotoPost }
+        />
+      )
+    })
+
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'Paulinha'}
-          fotoUsuario={FotoPerfil1} // Implementação Exercício 1
-          fotoPost={Post1}
+        <InputPost 
+          value={this.state.valorInputNome}
+          onChange={this.onChangeInputNome}
+          placeholder={'Nome de Usuário'}
         />
-                                                 
-        <Post
-          nomeUsuario={'João Pedro'}
-          fotoUsuario={FotoPerfil2}
-          fotoPost={Post2}
-         />
-        
-      const listaPosts = this.state.posts.map((post) => {
-      return (
-          <Post
-            nomeUsuario={'Daenerys'}
-            fotoUsuario={FotoPerfil3}
-            fotoPost={Post3}
-          />
 
-            nomeUsuario={post.nomeUsuario}
-            fotoUsuario={post.fotoUsuario}
-            fotoPost={post.fotoPost}
+        <InputPost
+          value={this.valorInputUsuario}
+          onChange={this.valorInputUsuario}
+          placeholder={'Foto de Perfil'}
+        />
 
-          //</div>>
-          //</Post>
-      );
-    });
+        <InputPost
+          value={this.valorInputPost}
+          onChange={this.valorInputPost}
+          placeholder={'Foto que deseja postar'}
+        />
 
-    return (
-      <div>
-        <div>{listaPosts}</div>
+        <ButtonAdd onClick={this.adicionarPost}>Postar</ButtonAdd>
+
+        {listaPost}
       </div>
     );
   }
 }
 
-
-
 export default App;
-
--- semana5/labegram-estilizacao-listas/src/components/Post/Post.js
+++ semana5/labegram-estilizacao-listas/src/components/Post/Post.js
@@ -8,106 +8,184 @@ import iconeCoracaoBranco from '../../img/favorite-white.svg'
 import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
 import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+
 import iconeFavBranco from '../../imagens/favoritewhite.svg'
 import iconeFavPreto from '../../imagens/favoriteblack.png'
+import {IconeMarcador} from '../IconeMarcador/IconeMarcador'
+import iconeShare from '../../img/share.png'
+import {SecaoCompartilhar} from '../SecaoCompartilhar/SecaoCompartilhar'
+
+import styled from 'styled-components'
+
+const PostContainer = styled.div`
+  border: 1px solid gray;
+  width: 300px;
+  margin-bottom: 10px;
+`
+const PostHeader = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding-left: 10px;
+`
+const PostFooter = styled.div`
+  height: 40px;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  justify-content: space-between;
+`
+
+const UserPhoto = styled.img`
+  height: 30px;
+  width: 30px;
+  margin-right: 10px;
+  border-radius: 50%;
+`
+
+const PostPhoto = styled.img`
+  width: 100%;
+`
 
 class Post extends React.Component {
   state = {
     curtido: false,
     numeroCurtidas: 0,
     comentando: false,
-    marcador: false,
-    numeroComentarios: 0
-   
+    numeroComentarios: 0,
+    marcado: false,
+    compartilhar: false
   }
 
-//  Implementações Exercícios2 - 3
+  onClickCurtida = () => {
+      this.setState({
+        curtido: true,
+        numeroCurtidas: this.state.numeroCurtidas + 1
+      })
 
-  onClickCurtida = () => {        // curtido = true ; componente renderizando
+      if (this.state.numeroCurtidas >= 1) {
+        this.setState({
+          curtido: false,
+          numeroCurtidas: this.state.numeroCurtidas - 1
+        })
+      }
+  }
+
+  onClickMarcado = () => {
     this.setState({
-      curtido: true,
-      numeroCurtidas: this.state.numeroCurtidas + 1
-    }) 
-      
-    if (this.state.numerocurtidas >= 1){
-      this.setState({
-        curtido: false,
-        numeroCurtidas: this.state.numeroCurtidas - 1
-     })
-   }
-      
-  }
-
-    onClickComentario = () => {  //componente renderizando
-      this.setState({
-      comentando: !this.state.comentando //comentando
+      marcado: true
     })
   }
 
-    aoEnviarComentario = () => {
+  onClickComentario = () => {
+    if(this.state.compartilhar) {
       this.setState({
-      comentando: false,
-      numeroComentarios: this.state.numeroComentarios + 1   //numerocomentarios ; componente renderizando
+        compartilhar: false
       })
     }
- 
-  /*onClickMarcador = () => {
-    
-    if (this.state.marcador) {
-        this.setState({marcador: true, callback: (iconeFavBranco) })
-       
-    } 
-        this.setState({marcador: false, callback: (iconeFavPreto) })
-  }*/
- 
-  render() {
+    this.setState({
+      comentando: !this.state.comentando
+    })
+  }
 
+  onClickCompartilhar = () => {
+    if(this.state.comentando) {
+      this.setState({
+        comentando: false
+      })
+    }
+    this.setState({
+      compartilhar: !this.state.compartilhar
+    })
+  }
+
+  aoEnviarComentario = () => {
+    this.setState({
+      comentando: false,
+      numeroComentarios: this.state.numeroComentarios + 1
+    })
+  }
+
+  aoCompartilhar = (event) => {
+    this.setState({
+      compartilhar: false
+    })
+    console.log('Post compartilhado no ' + event.target.value + ' com a mensagem: "Olha essa foto linda!"')
+  }
+
+
+  render() {
     let iconeCurtida
 
-    if(this.state.curtido) {  //curtido
+    if(this.state.curtido) {
       iconeCurtida = iconeCoracaoPreto
+      
     } else {
       iconeCurtida = iconeCoracaoBranco
     }
 
-   
+    let iconeMarcado
+
+    if(this.state.marcado) {
+      iconeMarcado = iconeFavPreto
+
+    } else {
+      iconeMarcado = iconeFavBranco
+
+    }
+
     let componenteComentario
 
     if(this.state.comentando) {
       componenteComentario = <SecaoComentario aoEnviar={this.aoEnviarComentario}/>
     }
 
-    return <div className={'post-container'}>
-      <div className={'post-header'}>
-        <img className={'user-photo'} src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
+    let componenteCompartilhar
+
+    if(this.state.compartilhar) {
+      componenteCompartilhar = (
+        <SecaoCompartilhar aoEnviar={this.aoCompartilhar} />
+      )
+    }
+
+    return <PostContainer>
+      <PostHeader>
+        <UserPhoto src={this.props.fotoUsuario} alt={'Imagem do usuario'}/>
         <p>{this.props.nomeUsuario}</p>
-      </div>
+      </PostHeader>
 
-      <img className={'post-photo'} src={this.props.fotoPost} alt={'Imagem do post'}/>
+      <PostPhoto src={this.props.fotoPost} alt={'Imagem do post'}/>
 
-      <div className={'post-footer'}>
+      <PostFooter>
+      
         <IconeComContador
-          icone={iconeCurtida} //componente que está em outra pasta que assim que clicado, ele registra a ação do usuário
-          onClickIcone={this.onClickCurtida} //classe
-          valorContador={this.state.numeroCurtidas} //classe numeroCurtidas
+          icone={iconeCurtida}
+          onClickIcone={this.onClickCurtida}
+          valorContador={this.state.numeroCurtidas}
         />
 
         <IconeComContador
           icone={iconeComentario}
           onClickIcone={this.onClickComentario}
-          valorContador={this.state.numeroComentarios} //numeroComentarios
+          valorContador={this.state.numeroComentarios}
         />
-
-      <IconeComContador
-          icone={iconeFavBranco|| iconeFavPreto}
-          onClickIcone={this.state.icone}
-      />
         
-      
-      </div>
+        <IconeMarcador 
+          icone={iconeShare}
+          onClickIcone={this.onClickCompartilhar}
+        />
+        
+        <IconeMarcador 
+          icone={iconeMarcado}
+          onClickIcone={this.onClickMarcado}
+        />
+        
+      </PostFooter>
       {componenteComentario}
-    </div>
+      {componenteCompartilhar}
+    </PostContainer>
   }
 }
+
 
 export default Post
