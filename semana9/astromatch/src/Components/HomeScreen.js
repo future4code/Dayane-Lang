ce3c84179d3e0264edb4f5a917e52ded7b81b6f7
dayknlang <dayaneknlang@gmail.com>
2021-04-07 20:58:10 -0300

    Projeto em andamento

--- semana9/astromatch/src/Components/HomeScreen.js
+++ semana9/astromatch/src/Components/HomeScreen.js
@@ -1,123 +1,139 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import { HomeConteiner, ContentScreen,  Header, TitleLogo, 
-   GoMathList, AnimationContainer, ProfileConteiner, ImageProfile,
-   NameProfile, AgeProfile, BioProfile, Footer, NoMatch, Match } from "./StyledPages/styledHome"
-//import ButtonNoMatch from '../images/reject-icon.svg';
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  HomeConteiner,
+  ContentScreen,
+  Header,
+  TitleLogo,
+  GoMathList,
+  AnimationContainer,
+  ProfileConteiner,
+  ImageProfile,
+  NameProfile,
+  AgeProfile,
+  BioProfile,
+  Footer,
+  NoMatch,
+  Match,
+} from "./StyledPages/styledHome";
+import ButtonNoMatch from "./images/iconNoMatch";
 //import NoMatchEffect from '../images/reject-colorful-icon.svg';
-//import ButtonMatch from '../images/accept-icon.svg';
+import ButtonMatch from "./images/iconMatch";
 //import MatchEffect from '../images/accept-colorful-icon.svg';
-//import MatchList from '../images/match-list.svg';
+import MatchList from "./images/iconfavorite.jpg";
 //import Logo from '../images/logo.svg';
-import Lottie from 'react-lottie';
-import animationData from '../Animation/animationHeart.json';
-
+import Lottie from "react-lottie";
+import animationData from "../Animation/animationHeart.json";
 
 function HomeScreen(props) {
-  const  [ choose, setChoose ] = useState(true)
-  const  [ noChoose, setNoChoose ] = useState(true)
-  const [ profiles, setProfiles ] = useState([])
-  const [ animationState, setAnimationState ] = useState({
-      isStopped: false, isPaused: false
-  })
+  const [choose, setChoose] = useState(true);
+  const [noChoose, setNoChoose] = useState(true);
+  const [profiles, setProfiles] = useState([]);
+  const [animationState, setAnimationState] = useState({
+    isStopped: false,
+    isPaused: false,
+  });
 
   const onMouseEvent = () => {
     if (choose) {
-      setChoose(false)
+      setChoose(false);
     } else {
-       setChoose(true)
-      }
-    };
-
-  const onMouseEffect = () => {
-    if (noChoose) {
-      setNoChoose(false)
-    } else {
-       setNoChoose(true)
-      }
-  };
-
-  const iconMatch = choose ? (ButtonMatch) : (MatchEffect);
-
-  const iconNoMatch = noChoose ? (ButtonNoMatch) : (NoMatchEffect);
-
-  
-  const defaultOptions = {
-    loop: true,
-    autoplay: true, 
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
+      setChoose(true);
     }
   };
 
+  const onMouseEffect = () => {
+    if (noChoose) {
+      setNoChoose(false);
+    } else {
+      setNoChoose(true);
+    }
+  };
+
+  const iconMatch = choose ? ButtonMatch : MatchEffect;
+
+  const iconNoMatch = noChoose ? ButtonNoMatch : NoMatchEffect;
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   useEffect(() => {
-     getProfiles()
-  }, [])
+    getProfiles();
+  }, []);
 
   const getProfiles = () => {
-    axios.get('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/dayane/person')
-    .then(response => {
-      setProfiles(response.data.profile)
-    })
-    .catch(error => {
-      console.log(error.message)
-    })
-  }
+    axios
+      .get(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/dayane/person"
+      )
+      .then((response) => {
+        setProfiles(response.data.profile);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
   const chooseProfile = () => {
     const body = {
       id: profiles.id,
-      choice: true
-    }
-    axios.post('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/dayane/choose-person',
-     body
-    )
-    .then(() => {
-      getProfiles()
-    })
-    .catch(error => {
-      console.log(error.message)
-    })
-  }
+      choice: true,
+    };
+    axios
+      .post(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/dayane/choose-person",
+        body
+      )
+      .then(() => {
+        getProfiles();
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
   const addMatch = () => {
-    chooseProfile()
-    alert('Perfil adicionado ♥')
-  }
+    chooseProfile();
+    alert("Perfil adicionado ♥");
+  };
 
   useEffect(() => {
-    rejectMatch()
-  }, [])
+    rejectMatch();
+  }, []);
 
   const rejectMatch = () => {
-    axios.get('https://us-central1-missao-newton.cloudfunctions.net/astroMatch/dayane/person')
-    .then(response => {
-      setProfiles(response.data.profile)
-    })
-    .catch(error => {
-      console.log(error.message)
-    })
-  }
+    axios
+      .get(
+        "https://us-central1-missao-newton.cloudfunctions.net/astroMatch/dayane/person"
+      )
+      .then((response) => {
+        setProfiles(response.data.profile);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
 
-
-  return (        
+  return (
     <HomeConteiner>
-      <ContentScreen className='home'>
+      <ContentScreen className="home">
         <Header>
-          <TitleLogo src={Logo}/>
-          <GoMathList
-            src={MatchList}
-            onClick={props.changeScreen}
-          />
+          <TitleLogo src={Logo} />
+          <GoMathList src={MatchList} onClick={props.changeScreen} />
         </Header>
-        <hr/>
+        <hr />
 
         {profiles.length === 0 ? (
           <AnimationContainer>
-            <Lottie options={defaultOptions}
+            <Lottie
+              options={defaultOptions}
               height={300}
               width={300}
               isStopped={animationState.isStopped}
@@ -126,32 +142,27 @@ function HomeScreen(props) {
           </AnimationContainer>
         ) : (
           <ProfileConteiner key={profiles.id}>
-            <ImageProfile src={profiles.photo}/>
+            <ImageProfile src={profiles.photo} />
             <NameProfile fontSize={24}>
-                {profiles.name}, 
-              <AgeProfile>
-                {profiles.age} anos
-              </AgeProfile>
+              {profiles.name},<AgeProfile>{profiles.age} anos</AgeProfile>
             </NameProfile>
-            <BioProfile>
-                {profiles.bio}
-            </BioProfile>
+            <BioProfile>{profiles.bio}</BioProfile>
           </ProfileConteiner>
         )}
 
         <Footer>
           <NoMatch
             src={iconNoMatch}
-            onClick={rejectMatch} 
+            onClick={rejectMatch}
             onMouseEnter={onMouseEffect}
             onMouseOut={onMouseEffect}
           />
           <Match
             src={iconMatch}
-            onClick={addMatch} 
+            onClick={addMatch}
             onMouseEnter={onMouseEvent}
             onMouseOut={onMouseEvent}
-          /> 
+          />
         </Footer>
       </ContentScreen>
     </HomeConteiner>
